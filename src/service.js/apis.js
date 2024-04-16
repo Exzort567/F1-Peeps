589faf31256b82f4c3237190cdeed6a2fba637b4
Kenneth <quibelkenneth@gmail.com>
2024-04-16 20:24:15 +0800

    try this fetching method

---
+++ src/service.js/apis.js
@@ -0,0 +1,199 @@
+export const grandprix = [
+    {
+        id: 1,
+        url: `https://ergast.com/api/f1/2024/1`
+        
+    },
+    {
+        id: 2,
+        url: `https://ergast.com/api/f1/2024/2`
+    },
+    {
+        id: 3,
+        url: `https://ergast.com/api/f1/2024/3`
+    },
+    {
+        id: 4,
+        url: `https://ergast.com/api/f1/2024/4`
+    },
+    {
+        id: 5,
+        url: `https://ergast.com/api/f1/2024/5`
+    },
+    {
+        id: 6,
+        url: `https://ergast.com/api/f1/2024/1`
+    },
+    {
+        id: 7,
+        url: `https://ergast.com/api/f1/2024/7`
+        
+    },
+    {
+        id: 8,
+        url: `https://ergast.com/api/f1/2024/8`
+    },
+    {
+        id: 9,
+        url: `https://ergast.com/api/f1/2024/9`
+    },
+    {
+        id: 10,
+        url: `https://ergast.com/api/f1/2024/10`
+    },
+    {
+        id: 11,
+        url: `https://ergast.com/api/f1/2024/11`
+    },
+    {
+        id: 12,
+        url: `https://ergast.com/api/f1/2024/12`
+    },
+    {
+        id: 13,
+        url: `https://ergast.com/api/f1/2024/13`
+        
+    },
+    {
+        id: 14,
+        url: `https://ergast.com/api/f1/2024/14`
+    },
+    {
+        id: 15,
+        url: `https://ergast.com/api/f1/2024/15`
+    },
+    {
+        id: 16,
+        url: `https://ergast.com/api/f1/2024/16`
+    },
+    {
+        id: 17,
+        url: `https://ergast.com/api/f1/2024/17`
+    },
+    {
+        id: 18,
+        url: `https://ergast.com/api/f1/2024/18`
+    },
+    {
+        id: 19,
+        url: `https://ergast.com/api/f1/2024/19`
+    },
+    {
+        id: 20,
+        url: `https://ergast.com/api/f1/2024/20`
+    },
+    {
+        id: 21,
+        url: `https://ergast.com/api/f1/2024/21`
+    },
+    {
+        id: 22,
+        url: `https://ergast.com/api/f1/2024/22`
+    },
+    {
+        id: 23,
+        url: `https://ergast.com/api/f1/2024/23`
+    }
+];
+
+
+export const raceresult = [
+    {
+        id: 1,
+        
+        url: `http://ergast.com/api/f1/current/1/results`
+        
+    },
+    {
+        id: 2,
+        url: `http://ergast.com/api/f1/current/2/results`
+    },
+    {
+        id: 3,
+        url: `http://ergast.com/api/f1/current/3/results`
+    },
+    {
+        id: 4,
+        url: `http://ergast.com/api/f1/current/4/results`
+    },
+    {
+        id: 5,
+        url: `http://ergast.com/api/f1/current/5/results`
+    },
+    {
+        id: 6,
+        url: `http://ergast.com/api/f1/current/6/results`
+    },
+    {
+        id: 7,
+        url: `http://ergast.com/api/f1/current/7/results`
+        
+    },
+    {
+        id: 8,
+        url: `http://ergast.com/api/f1/current/8/results`
+    },
+    {
+        id: 9,
+        url: `http://ergast.com/api/f1/current/9/results`
+    },
+    {
+        id: 10,
+        url: `http://ergast.com/api/f1/current/10/results`
+    },
+    {
+        id: 11,
+        url: `http://ergast.com/api/f1/current/11/results`
+    },
+    {
+        id: 12,
+        url: `http://ergast.com/api/f1/current/12/results`
+    },
+    {
+        id: 13,
+        url: `http://ergast.com/api/f1/current/13/results`
+        
+    },
+    {
+        id: 14,
+        url: `http://ergast.com/api/f1/current/14/results`
+    },
+    {
+        id: 15,
+        url: `http://ergast.com/api/f1/current/15/results`
+    },
+    {
+        id: 16,
+        url: `http://ergast.com/api/f1/current/16/results`
+    },
+    {
+        id: 17,
+        url: `http://ergast.com/api/f1/current/17/results`
+    },
+    {
+        id: 18,
+        url: `http://ergast.com/api/f1/current/18/results`
+    },
+    {
+        id: 19,
+        url: `http://ergast.com/api/f1/current/19/results`
+    },
+    {
+        id: 20,
+        url: `http://ergast.com/api/f1/current/20/results`
+    },
+    {
+        id: 21,
+        url: `http://ergast.com/api/f1/current/21/results`
+    },
+    {
+        id: 22,
+        url: `http://ergast.com/api/f1/current/22/results`
+    },
+    {
+        id: 23,
+        url: `http://ergast.com/api/f1/current/23/results`
+    }
+];
+
+
